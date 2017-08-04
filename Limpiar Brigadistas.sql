d6ac1e3562bae7341b239e9117f8a5b036375d53
Ignacio Martinez <nachomartinez3010@gmail.com>
2017-08-04 11:19:34 -0500

    Version 1.9.1 distribuibles

--- Limpiar Brigadistas.sql
+++ Limpiar Brigadistas.sql
@@ -1,1 +1,2 @@
+UPDATE BRIGADA_Brigadistas SET  PuestoID = 1 WHERE Nombre="David" AND ApellidoPaterno="Bautista" AND ApellidoMaterno="Paez" OR Nombre="Joaquín" AND ApellidoPaterno="García" AND ApellidoMaterno="Barrios"; 
 UPDATE BRIGADA_Brigadistas    SET Nombre = ltrim(rtrim(Nombre)), ApellidoMaterno = ltrim(rtrim(ApellidoMaterno)), ApellidoPaterno = ltrim(rtrim(ApellidoPaterno)) ;
